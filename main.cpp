cace941aa53281d0274f6c4772c17ffe1083c778
BreadBurgIar <ericcom@outlook.com>
2023-10-18 00:18:07 -0400

    Finished part 1 (holy shit that was a lot)

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <fstream>
 #include <vector>
+#include <sstream>
 
 using namespace std;
 
@@ -10,22 +11,34 @@ int main()
     ifstream input;
     input.open("./input");
 
-    vector<string> data;
+    vector<int> data;
+    stringstream ss;
     string line;
 
     if (input.is_open())
     {
         while (getline(input, line))
         {
-            data.push_back(line);
+            ss << line;
+            int n;
+            ss >> n;
+            ss.clear();
+            data.push_back(n);
         }
     }
     input.close();
-
+    
     for (int i = 0; i < data.size(); i++)
     {
-        cout << data[i] << endl;
+        for (int j = 0; j < data.size(); j++)
+        {
+            if (data[i] + data[j] == 2020)
+            {
+                cout << data[i] * data[j] << endl;
+                return 0;
+            }
+        }
     }
 
-    return 0;
+    return -1;
 }
